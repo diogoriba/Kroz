12fac8933f670eed5a66fd893cf03117f747b51f
hellwilliam <gbourscheidt@hotmail.com>
2015-10-08 20:15:57 -0300

    Mapa do jogo

---
+++ Assets/Server/GameObjects/Map.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Server.GameObjects
+{
+    public class Map : Item
+    {
+       string map01 = "***********                                                ";
+       string map02 = "*         *                                                ";
+       string map03 = "*         *            ******                              ";
+       string map04 = "********  *            *    *                              ";
+       string map05 = "*         *            *    *                              ";
+       string map06 = "*         *          ****——****            ****************";
+       string map07 = "*         *          *        *            *              *";
+       string map08 = "*         ********   *        *            *              *";
+       string map09 = "*         *      *****        *            *              *";
+       string map10 = "*         |      |   |        *            *              *";
+       string map11 = "*         *      *****        *            *              *";
+       string map12 = "*         *      *   *        *            *              *";
+       string map13 = "*************——***   *        *            *              *";
+       string map14 = "            *  *     ****——****            *              *";
+       string map15 = "            *  *        *  *               *              *";
+       string map16 = "            *  *        *  *               *              *";
+       string map17 = "            *  *        *  *****************              *";
+       string map18 = "            *  *        *  ||||||||||||||» |              *";
+       string map19 = "            *  *        ***********************************";
+       string map20 = "            *  *                                           ";
+       string map21 = "            *  *                                           ";
+       string map22 = "            *  *                                           ";
+       string map23 = "            *  *                                           ");
+    }   
+}
